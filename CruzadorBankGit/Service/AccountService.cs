2cec441aec80669f25cde94f22e950cf6b8c56ef
Vrooch <vinirooch@gmail.com>
2026-08-30 15:34:40 -0300

    feat: add password to account entity and implemented password service

--- CruzadorBankGit/Service/AccountService.cs
+++ CruzadorBankGit/Service/AccountService.cs
@@ -1,29 +1,40 @@
-﻿using CruzadorBankGit.Repository;
-using CruzadorBankGit.Entity;
+﻿using CruzadorBankGit.Entity;
+using CruzadorBankGit.Repository;
+using Konscious.Security.Cryptography;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace CruzadorBankGit.Service
 {
-    internal class AccountService
+    internal class AccountService : IPasswordService
     {
         private readonly AccountRepository _accountRepository;
+        public int DegreeOfParallelism = 4; // quantidade de trabalho paralelo
+        public int MemorySize = 65536; // Custo de memoria --> aprox 64mb
+        public int Iterations = 3; // Numero de ciclos
 
         public AccountService()
         {
             _accountRepository = new AccountRepository();
         }
-        public void CreateAccount(string name, decimal balance)
+        public void CreateAccount(string name, decimal balance, string password)
         {
             if(balance < 0) throw new ArgumentOutOfRangeException(nameof(balance), "Balance should be equals ou bigger than 0");
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name), "Name should be a valid, not null, empty or white Space message");
 
+            PasswordContentVerifier(password);
+
+            byte[] salt = RandomNumberGenerator.GetBytes(16);
+
+            byte[] HashedPassword = PasswordHasher(password, salt);
+
             int currentId = _accountRepository.GetCurrentId();
             int newId = ++currentId;
 
-            Account account = new Account(newId, name, balance);
+            Account account = new Account(newId, name, balance, HashedPassword, salt);
 
             _accountRepository.SetNewId(newId);
 
@@ -40,6 +51,33 @@ namespace CruzadorBankGit.Service
         {
             Account account = GetAccount(name, accountId);
             return new ArrayList { account.AccountId, account.Name, account.Balance };
+        }
+
+        public void PasswordContentVerifier(string password)
+        {
+            if (string.IsNullOrEmpty(password)) throw new ArgumentNullException(nameof(password), "Password should not be null or empty");
+            if (password.Length <= 5) throw new ArgumentOutOfRangeException(nameof(password), "The password should be bigger than 5 character");
+            if (!password.Any(char.IsUpper)) throw new ArgumentException(nameof(password), "Password should have at least one uppert case letter");
+            //adicionar novas regras de composicao de senhas
+        }
+
+        public byte[] PasswordHasher(string password, byte[] salt)
+        {
+            using (Argon2id argon = new Argon2id(Encoding.UTF8.GetBytes(password)))
+            {
+                argon.Salt = salt;
+                argon.DegreeOfParallelism = this.DegreeOfParallelism;
+                argon.MemorySize = this.MemorySize;
+                argon.Iterations = this.Iterations;
+
+                return argon.GetBytes(32);
+            }
+        }
+
+        public bool PasswordVerify(string password, byte[] currentPassword, byte[] salt)
+        {
+            byte[] HashedPassword = PasswordHasher(password, salt);
+            return CryptographicOperations.FixedTimeEquals(HashedPassword, currentPassword);
         }
     }
 }
---
+++ CruzadorBankGit/Service/IPasswordService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CruzadorBankGit.Service
+{
+    internal interface IPasswordService
+    {
+        public byte[] PasswordHasher(string password, byte[] salt);
+        public bool PasswordVerify(string password, byte[] currentPassword, byte[] salt);
+        public void PasswordContentVerifier(string password); // void pq a verificacao deve acontecer com base no lancamento de excecoes
+    }
+}
